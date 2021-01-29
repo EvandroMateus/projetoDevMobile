67dc63964b492c807f8f98010b5bbdcf96624f42
Evandro Mateus <evandromateus06@gmail.com>
2021-01-29 20:41:52 -0300

    cadastro e listagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ContentAddComponent } from './content-add/content-add.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'tabs/tab1/adicionar-editar',
+    component: ContentAddComponent
+  },
+  {
+    path: 'tabs/tab2/adicionar-editar',
+    component: ContentAddComponent
   }
 ];
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicStorageModule } from '@ionic/storage'
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -8,11 +11,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentAddComponent } from './content-add/content-add.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,
+    ContentAddComponent  
+  ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
